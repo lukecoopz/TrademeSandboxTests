10db53392807a8d6a94f668ee1880fb224c2fee1
lukecoopz <lukecoopz@gmail.com>
2024-06-05 20:02:23 +1200

    wip

--- specs/categories.spec.ts
+++ specs/categories.spec.ts
@@ -2,9 +2,15 @@ import * as supertest from 'supertest'
 const request = supertest('https://api.trademe.co.nz/v1/Categories')
 
 describe('GET REQUESTS', () =>{
-    it('GET /posts', async () => {
+    it('GET xml file', async () => {
         const response = await request.get('.xml')
         // 200 OK
         expect(response.status).toBe(200)
+    })
+    it('GET json file with subcategory', async () => {
+        const response = await request.get('/0001-.json')
+        // 200 OK
+        expect(response.status).toBe(200)
+        expect(response.body.Name).toBe('Trade Me Motors')
     })
 })
