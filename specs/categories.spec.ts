9b63ee028f1ab72de7c8fbd7f36636b41995f367
lukecoopz <lukecoopz@gmail.com>
2024-06-05 20:09:43 +1200

    wip

--- specs/categories.spec.ts
+++ specs/categories.spec.ts
@@ -7,10 +7,27 @@ describe('GET REQUESTS', () =>{
         // 200 OK
         expect(response.status).toBe(200)
     })
-    it('GET json file with subcategory', async () => {
+    it('GET subcategory for Trade Me Motors', async () => {
         const response = await request.get('/0001-.json')
         // 200 OK
         expect(response.status).toBe(200)
         expect(response.body.Name).toBe('Trade Me Motors')
+    })
+    it('GET subcategory for Computers', async () => {
+        const response = await request.get('/0002-.json')
+        // 200 OK
+        expect(response.status).toBe(200)
+        expect(response.body.Name).toBe('Computers')
+    })
+    it('GET subcategory for Movies & TV', async () => {
+        const response = await request.get('/0003-.json')
+        // 200 OK
+        expect(response.status).toBe(200)
+        expect(response.body.Name).toBe('Movies & TV')
+    })
+    it('Returns 400 with no subcategory', async () => {
+        const response = await request.get('/999-.json')
+        // 400 no category that is 999
+        expect(response.status).toBe(400)
     })
 })
